6415a87c1a1369b147f6b40c65b7b211c605f25e
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2024-01-01 11:31:34 +0700

    Update FragmentBusiness.kt

--- berita/app/src/main/java/com/walwiyo/newsapp/fragment/FragmentBusiness.kt
+++ berita/app/src/main/java/com/walwiyo/newsapp/fragment/FragmentBusiness.kt
@@ -7,27 +7,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.azhar.newsapp.R
-import com.azhar.newsapp.adapter.NewsAdapter
-import com.azhar.newsapp.model.ModelArticle
-import com.azhar.newsapp.model.ModelNews
-import com.azhar.newsapp.networking.ApiEndpoint.getApiClient
-import com.azhar.newsapp.networking.ApiInterface
-import com.azhar.newsapp.util.Utils.getCountry
+import com.walwiyo.berita.R
+import com.walwiyo.adapter.NewsAdapter
+import com.walwiyo.adapter.model.ModelArticle
+import com.walwiyo.adapter.model.ModelNews
+import com.walwiyo.adapter.networking.ApiEndpoint.getApiClient
+import com.walwiyo.adapter.networking.ApiInterface
+import com.walwiyo.adapter.util.Utils.getCountry
 import kotlinx.android.synthetic.main.fragement_news.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
 
-/**
- * Created by Azhar Rivaldi on 10-04-2021
- * Github : https://github.com/AzharRivaldi
- * Linkedin : https://www.linkedin.com/in/azhar-rivaldi
- * Instagram : https://www.instagram.com/azhardvls_
- * Twitter : https://twitter.com/azharrvldi_
- * Youtube Channel : https://bit.ly/2PJMowZ
- */
+
 
 class FragmentBusiness : Fragment() {
 
