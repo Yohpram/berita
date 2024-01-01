ee0e2de680424a4f65e5ecdc51406a9314a60fc8
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2024-01-01 11:28:11 +0700

    Update NewsAdapter.kt

--- berita/app/src/main/java/com/walwiyo/newsapp/adapter/NewsAdapter.kt
+++ berita/app/src/main/java/com/walwiyo/newsapp/adapter/NewsAdapter.kt
@@ -1,4 +1,4 @@
-package com.azhar.newsapp.adapter
+package com.walwiyo.berita.adapter
 
 import android.content.Context
 import android.content.Intent
@@ -8,24 +8,17 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.azhar.newsapp.R
-import com.azhar.newsapp.activities.DetailNewsActivity
-import com.azhar.newsapp.model.ModelArticle
-import com.azhar.newsapp.util.Utils.DateFormat
-import com.azhar.newsapp.util.Utils.DateTimeHourAgo
+import com.walwiyo.berita.R
+import com.walwiyo.berita.activities.DetailNewsActivity
+import com.walwiyo.berita.model.ModelArticle
+import com.walwiyo.berita.util.Utils.DateFormat
+import com.walwiyo.berita.util.Utils.DateTimeHourAgo
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.makeramen.roundedimageview.RoundedImageView
 import kotlinx.android.synthetic.main.list_item_news.view.*
 
-/**
- * Created by Azhar Rivaldi on 10-04-2021
- * Github : https://github.com/AzharRivaldi
- * Linkedin : https://www.linkedin.com/in/azhar-rivaldi
- * Instagram : https://www.instagram.com/azhardvls_
- * Twitter : https://twitter.com/azharrvldi_
- * Youtube Channel : https://bit.ly/2PJMowZ
- */
+
 
 class NewsAdapter(private val modelArticles: MutableList<ModelArticle>, private val context: Context) :
         RecyclerView.Adapter<ViewHolder>() {
