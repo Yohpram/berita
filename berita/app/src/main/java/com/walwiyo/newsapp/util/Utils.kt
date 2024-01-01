eba9866e9bca6b4bc4a83552a17b49e63024aff7
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2024-01-01 11:26:19 +0700

    Update Utils.kt

--- berita/app/src/main/java/com/walwiyo/newsapp/util/Utils.kt
+++ berita/app/src/main/java/com/walwiyo/newsapp/util/Utils.kt
@@ -1,18 +1,11 @@
-package com.azhar.newsapp.util
+package com.yohanes.berita.util
 
 import org.ocpsoft.prettytime.PrettyTime
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
-/**
- * Created by Azhar Rivaldi on 10-04-2021
- * Github : https://github.com/AzharRivaldi
- * Linkedin : https://www.linkedin.com/in/azhar-rivaldi
- * Instagram : https://www.instagram.com/azhardvls_
- * Twitter : https://twitter.com/azharrvldi_
- * Youtube Channel : https://bit.ly/2PJMowZ
- */
+
 
 object Utils {
 
