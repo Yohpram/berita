ee03f1143c8cf534f7ae4bede7e8d58c8da8d577
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2024-01-01 11:49:55 +0700

    Update ModelArticle.kt

--- berita/app/src/main/java/com/walwiyo/newsapp/model/ModelArticle.kt
+++ berita/app/src/main/java/com/walwiyo/newsapp/model/ModelArticle.kt
@@ -1,16 +1,9 @@
-package com.azhar.newsapp.model
+package com.walwiyo.berita.model
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
-/**
- * Created by Azhar Rivaldi on 10-04-2021
- * Github : https://github.com/AzharRivaldi
- * Linkedin : https://www.linkedin.com/in/azhar-rivaldi
- * Instagram : https://www.instagram.com/azhardvls_
- * Twitter : https://twitter.com/azharrvldi_
- * Youtube Channel : https://bit.ly/2PJMowZ
- */
+
 
 data class ModelArticle(
         @SerializedName("source")
